d8203aa1eb20e0fd93b35c8c0ab7c434c649a1a5
Christopher Villalobos <christopher.villalobos@gmail.com>
2011-03-30 17:08:29 -0400

    Starting broken graph support
    Signed-off-by: Christopher Villalobos <christopher.villalobos@gmail.com>

--- js/oac-nationalstats.js
+++ js/oac-nationalstats.js
@@ -1,3 +1,41 @@
+(function(nil) {
+    OACGraph.implement('undeflinechart', function(x, y, w, h, xdata, ydata, opts) {
+        ydata = (typeOf(ydata[0]) === 'array') ? ydata[0] : ydata;
+        // xdata info
+        var xdim = this.paper.g.snapEnds(0,ydata.length, ydata.length-1),
+            minx = xdim.from,
+            maxx = xdim.to,
+            cleaned = [],
+            kx = ( w - this.options.chartOptions.gutter * 2) / ((maxx - minx) || 1);
+        console.log(xdim);
+        for( var i=0, l=ydata.length, offset=0, cd=[]; i < l; i++ ) {
+            if((ydata[i] === undefined) || (ydata[i] === null)) {
+                offset++;
+                if(cd.length > 0)
+                    cleaned.push(cd);
+                cd = [];
+            } else {
+                if(offset !== 0)
+                    cleaned.push(offset);
+                cd.push(ydata[i]);
+                offset = 0;
+            }
+        }
+        for( var i=0, l = cleaned.length, offset=0; i < l; i++ ) {
+            if(typeOf(cleaned[i]) === 'array') {
+                offset += cleaned[i].length;
+                this.paper.path('M'+(x+ this.options.chartOptions.gutter + offset*kx)+" 0 L"+(x+ this.options.chartOptions.gutter + offset*kx)+" 1000");
+            } else {
+                offset += cleaned[i];
+                this.paper.path('M'+(x+ this.options.chartOptions.gutter + offset*kx)+" 0 L"+(x+ this.options.chartOptions.gutter + offset*kx)+" 1000");
+            }
+        }
+        console.log(cleaned);
+        if( xdata === undefined ) xdata = [].range(0, ydata.length-1);
+        return this.paper.g.linechart(x, y, w, h, [xdata], ydata, opts);
+    });
+}).call(this);
+
 var OACNationalStats = new Class({
     Implements: [Options],
     
@@ -15,7 +53,7 @@ var OACNationalStats = new Class({
 			element: document.id( 'commodity-graph' ), 
 			height: 300,
 			width: 600,
-			type: 'linechart',
+			type: 'undeflinechart',
 			chartOptions: {
 				centeraxis: false,
 				shade: true,
@@ -80,7 +118,9 @@ var OACNationalStats = new Class({
 		this.graph.options.labels = years;
 		this.graph.options.max = data.max();
 		this.graph.options.min = 0;
+		this.graph.options.graphOptions.xlabel = rawdata.xlabel;
 		this.graph.redraw( data, true, years );
+		// Add a color ENSO hack later
 	}
 });
 
